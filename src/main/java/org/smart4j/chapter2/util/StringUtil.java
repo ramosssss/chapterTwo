f4e20ba20258c2d099c426ec5cae3293d3957c6a
ramosssss <946073945@qq.com>
2018-04-18 18:58:06 +0800

    《架构探险》 第二章代码 2018年4月18日 18点56分

---
+++ src/main/java/org/smart4j/chapter2/util/StringUtil.java
@@ -0,0 +1,23 @@
+package org.smart4j.chapter2.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+public final class StringUtil {
+
+    /*
+    * 判断字符串是否为空
+    * */
+    public static boolean isEmpty(String str) {
+        if (str != null)
+            str = str.trim();
+        return StringUtils.isEmpty(str);
+    }
+
+    /*
+    * 判断字符串是否非空
+    * */
+
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+}
